684c80a56891328967c6ebd68d0904ec991c0c0b
rizzle-rohit <rohit.y@thesilverlabs.com>
2022-02-18 17:51:34 +0530

    added interval

--- server2.js
+++ server2.js
@@ -2,28 +2,50 @@ const express = require('express');
 const axios = require('axios')
 const app = express();
 
-app.get('/' , async()=>{
-   const res =   await axios({
-        url: 'https://beta.thesilverlabs.com/graphql',
-        method: 'GET',
-        data: {
-          query: `{
+app.get('/' , async(req , res)=>{
   
-  
-  
-            aboutUsSectionOld{
-             name
-           }
-             
-             
-           
-           }
-            `
-        }
-      })
-      console.log(JSON.stringify(res.data));
+  res.send("hello server 2");
         
 })
-app.listen(4000 , ()=>{
+
+
+
+  setInterval(async()=>{
+
+    const res =   await axios({
+      url: 'https://beta.thesilverlabs.com/graphql',
+      method: 'GET',
+      data: {
+        query: `{
+  
+  
+  
+          aboutUsSectionOld{
+           name
+         }
+           
+           
+         
+         }
+          `
+      }
+    })
+    if(res.status!=200)
+    console.log(res.status);
+  
+
+
+
+
+  },5000)
+
+
+
+
+
+app.listen(4000 , async ()=>{
+
+  
+
     console.log("server2 running on port 4000");
 })
